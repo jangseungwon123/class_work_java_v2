639f5e2ba77e97e750c0dda0add470d8281c41c2
jang seungwon <rhanwnf97@naver.com>
2025-05-27 11:53:53 +0900

    백업용 히카리

--- java_hikariCP/src/main/java/com/tenco/Main.java
+++ java_hikariCP/src/main/java/com/tenco/Main.java
@@ -1,4 +1,10 @@
 package com.tenco;
 
 public class Main {
-}
+
+    public static void main(String[] args) {
+
+        System.out.println("UTP-8 인코딩 확인");
+
+    } // end of main
+} // end of class
--- java_hikariCP/src/main/java/com/tenco/dto/Book.java
+++ java_hikariCP/src/main/java/com/tenco/dto/Book.java
@@ -1,4 +1,23 @@
 package com.tenco.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
+
+
+        private int id;
+        private String title;
+        private String author;
+        private String publisher;
+        private int publicationYear;
+        private String isbn;
+        private boolean available;
+
+
+
 }
